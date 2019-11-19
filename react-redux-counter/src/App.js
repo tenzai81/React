070f640a69c0de484e59fcb72ceb564713e0505d
Huy Nguyen <tenzai81@gmail.com>
2019-11-19 11:29:44 -0600

    increment working

--- react-redux-counter/src/App.js
+++ react-redux-counter/src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import CounterDisplay from "./components/CounterDisplay";
 
 function App() {
   return (
     <Provider store={store}>
-      <h2>Hello</h2>
+      <h2>
+        <CounterDisplay />
+      </h2>
     </Provider>
   );
 }
