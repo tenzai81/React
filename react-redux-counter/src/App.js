66eec3e8d84a14d79e14d7df198a833ed9a4368e
Huy Nguyen <tenzai81@gmail.com>
2019-11-19 11:08:37 -0600

    first commit

--- react-redux-counter/src/App.js
+++ react-redux-counter/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    
+    <Provider store={store}>
+      <h2>Hello</h2>
+    </Provider>
   );
 }
 
